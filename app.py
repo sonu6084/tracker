001b75ed1f323272c61f520bef890e6fb6beb2f6
sonu6084 <nigam6084@gmail.com>
2025-09-16 14:54:42 +0530

    Initial Flask app with templates folder

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+def load_data():
+    df = pd.read_csv("data.csv")
+    return df.to_dict(orient="records")
+
+@app.route("/")
+def index():
+    data = load_data()
+    return render_template("index.html", data=data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
